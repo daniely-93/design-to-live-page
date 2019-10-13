06e5e3943b72dd1fbffae7039a9bbd6f4ccfc88a
daniely-93 <danielyona93@gmail.com>
2019-10-13 16:11:21 +0300

    Add JS and dynamic data

---
+++ js/posts-controller.js
@@ -0,0 +1,18 @@
+renderPosts();
+
+function renderPosts(){
+    var posts = getPosts();
+    var strHTMLs = posts.map(post => {
+        return `<section class="card flex">
+            <img class="card-img" src="${post.imgUrl}" alt="${post.title}" />
+            <div class="card-content space-between flex wrap">
+                <h2 class="card-title">${post.title}</h2>
+                <p class="card-text">${post.text1}</p>
+                <p class="card-text">${post.text2}</p>
+                <button class="card-continue-btn" type="button">Continue Reading</button>
+            </div>
+        </section>`
+    }).join('');
+    console.log(strHTMLs);
+    document.querySelector('.card-container').innerHTML = strHTMLs;
+}
---
+++ js/posts-service.js
@@ -0,0 +1,36 @@
+const POSTS_KEY = 'posts';
+
+var gPosts = createPosts();
+
+function createPosts() {
+    return [
+        createPost('https://i.imgur.com/G378rDS.jpg',
+            'DUIS AUTE IRURE DOLOR IN HENDERIT IN VOLUPTATE.',
+            'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commo doconsequat, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur...'
+        ),
+        createPost('https://i.imgur.com/KFVpL64.jpg',
+            'DUIS AUTE IRURE DOLOR IN HENDERIT IN VOLUPTATE.',
+            'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commo doconsequat, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur...'
+        ),
+        createPost('https://i.imgur.com/Mb76qqz.jpg',
+            'DUIS AUTE IRURE DOLOR IN HENDERIT IN VOLUPTATE.',
+            'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commo doconsequat, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur...'
+        )
+    ]
+}
+
+function createPost(imgUrl, title, text1, text2) {
+    return {
+        imgUrl,
+        title,
+        text1,
+        text2
+    }
+}
+
+function getPosts() {
+    return gPosts;
+}
